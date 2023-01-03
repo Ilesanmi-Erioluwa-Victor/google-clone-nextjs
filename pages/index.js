53c440f9bf525ef1eb56cc64c3abc3b276cae4fc
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-03 02:02:51 +0100

    Working on header.js section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import Header from '@/components/Header'
 
 export default function Home() {
   return (
@@ -10,9 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-       <main>
-<h2 className='text-yellow-500'>Hello world..</h2>
-       </main>
+     {/* Header section */}
     </>
   )
 }
