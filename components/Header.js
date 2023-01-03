1339addb991a348414dd1bf947a8f89948566e4c
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-03 02:39:30 +0100

    Still on header

--- components/Header.js
+++ components/Header.js
@@ -4,15 +4,15 @@ import User from './User'
 const Header = () => {
   return (
     <header className='flex justify-between items-center p-5 text-sm text-gray-700'>
-      <section className=''>
-        <ul>
+      <section className='block relative'>
+        <ul className='flex space-x-4 items-center'>
             <li>About</li>
             <li>Store</li>
         </ul>
       </section>
 
       <section>
-      <ul>
+      <ul className='flex space-x-4 items-center'>
             <li>Gmail</li>
             <li>Images</li>
             <User />
